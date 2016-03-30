25d98360e70cc7b27a9451617df9729cdc01617b
weaamhafez <weaam.hafez@gmail.com>
2016-03-30 22:31:07 +0400

    Missing file

---
+++ Engineer.EMF/App_Code/Models/UserStoryData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engineer.EMF.Models
+{
+    [Serializable]
+    public class UserStoryData
+    {
+        public string ProjectId { set; get; }
+        public List<string>  Users { set; get; }
+    }
+}
